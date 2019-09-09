0bd5d6bec22017da4c338f2ad9be6adf6a7cf214
hungryyang <ycr1714336458@gmail.com>
2019-09-09 15:33:35 +0800

    npm配置

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from './button/button';
-import Icon from './icon/icon';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Button from './button/button';
+// import Icon from './icon/icon';
 
-const handlerClick = (event: React.MouseEvent<HTMLOrSVGElement>) => {
-    console.log(event.target)
-}
+export {default as Icon} from  './icon/icon'
+export {default as Button} from  './button/button'
 
-ReactDOM.render(
-    <div>
-        <Button icon="add" onClick={handlerClick} className="j">测试1</Button>
-        <Button icon="left" onClick={handlerClick} className="j">测试1</Button>
-        <Button icon="download"  iconPosition="right" onClick={handlerClick} className="j">测试1</Button>
-        <Button icon="up" onClick={handlerClick} className="j">测试1</Button>
-
-        <Icon name="add"  onClick={handlerClick} onMouseEnter={()=>{console.log('mouseEnter')}}/>
-
-        <Icon name="right" className="test-icon test-icon-2" onClick={handlerClick} onMouseEnter={()=>{console.log('mouseEnter')}}/>
-    </div>,
-    document.getElementById('root')
-);
+// const handlerClick = (event: React.MouseEvent<HTMLOrSVGElement>) => {
+//     console.log(event.target)
+// }
+//
+// ReactDOM.render(
+//     <div>
+//         <Button icon="add" onClick={handlerClick} className="j">测试1</Button>
+//         <Button icon="left" onClick={handlerClick} className="j">测试1</Button>
+//         <Button icon="download"  iconPosition="right" onClick={handlerClick} className="j">测试1</Button>
+//         <Button icon="up" onClick={handlerClick} className="j">测试1</Button>
+//
+//         <Icon name="add"  onClick={handlerClick} onMouseEnter={()=>{console.log('mouseEnter')}}/>
+//
+//         <Icon name="right" className="test-icon test-icon-2" onClick={handlerClick} onMouseEnter={()=>{console.log('mouseEnter')}}/>
+//     </div>,
+//     document.getElementById('root')
+// );
